7add99cdfb1f05ec656bfd9ca99bd15997a8e2d1
ishanawasthioffical-spec <ishanawasthioffical@gmail.com>
2026-06-22 13:05:06 +0530

    work

---
+++ WEEK 3/day 20/2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    int A[r][c];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> A[i][j];
+
+    cout << "Row-wise Sum:\n";
+
+    for(int i = 0; i < r; i++) {
+        int sum = 0;
+
+        for(int j = 0; j < c; j++)
+            sum += A[i][j];
+
+        cout << "Row " << i + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
---
+++ WEEK 3/day 20/3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    int A[r][c];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> A[i][j];
+
+    cout << "Column-wise Sum:\n";
+
+    for(int j = 0; j < c; j++) {
+        int sum = 0;
+
+        for(int i = 0; i < r; i++)
+            sum += A[i][j];
+
+        cout << "Column " << j + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
